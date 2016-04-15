ed6e2f370e3bb18bf78e8a33d34bbb35d1ff55e6
structure7 <kemperm@usa.net>
2016-04-14 22:55:01 -0700

    Update fridgeTemps.ino

--- fridgeTemps.ino
+++ fridgeTemps.ino
@@ -13,22 +13,22 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#define ONE_WIRE_BUS 12 // NodeMCU D6, ESP-12E GPIO12 (WORKS!)
+#define ONE_WIRE_BUS 12 // NodeMCU pin D6, ESP-12E GPIO12
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-DeviceAddress ds18b20Fridge = { 0x28, 0x77, 0x8A, 0x1E, 0x00, 0x00, 0x80, 0xD4 }; // Refridgerator
+DeviceAddress ds18b20Fridge = { 0x28, 0x77, 0x8A, 0x1E, 0x00, 0x00, 0x80, 0xD4 }; // Refrigerator
 DeviceAddress ds18b20Freezer = { 0x28, 0xBE, 0x89, 0x1E, 0x00, 0x00, 0x80, 0x93 }; // Freezer
 
 void setup()
 {
-  Wire.begin(4, 5); // Set pins for SDA/SCL (4=D2 for SDA and 5=D1 for SCL)
+  Wire.begin(4, 5); // Set pins for SDA/SCL (GPIO4 = pin D2 for SDA, and GPIO5 = pin D1 for SCL)
   sensors.begin();
   sensors.setResolution(ds18b20Fridge, 9);
   sensors.setResolution(ds18b20Freezer, 9);
 
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // Originally 0x3D
 }
 
 void loop()
