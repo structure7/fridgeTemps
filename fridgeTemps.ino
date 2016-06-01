a1dddc46779d125f0bcc1a70034e9f08c8f28a3e
structure7 <kemperm@usa.net>
2016-06-01 13:38:50 -0700

    Optos

--- fridgeTemps.ino
+++ fridgeTemps.ino
@@ -1,22 +1,23 @@
+#include <SimpleTimer.h>
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-
-
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <WidgetRTC.h>
 #include <Time.h>
 #include <TimeLib.h>
-#include <SimpleTimer.h>
-
-char auth[] = "fromBlynkApp";
-SimpleTimer timer;
-
 #include <Wire.h>
 #include <SPI.h>
+WidgetTerminal terminal(V26);
+
+char auth[] = "fromBlynkApp";
+
+SimpleTimer timer;
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#define OLED_RESET 0
+#define OLED_RESET 15
 Adafruit_SSD1306 display(OLED_RESET);
 
 #define NUMFLAKES 10
@@ -24,15 +25,15 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define YPOS 1
 #define DELTAY 2
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#define ONE_WIRE_BUS 12 // NodeMCU D6, ESP-12E GPIO12 (WORKS!)
-
+#define ONE_WIRE_BUS 0 // WeMos pin D3 w/ pull-up
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 DeviceAddress ds18b20Fridge = { 0x28, 0x77, 0x8A, 0x1E, 0x00, 0x00, 0x80, 0xD4 }; // Refridgerator
 DeviceAddress ds18b20Freezer = { 0x28, 0xBE, 0x89, 0x1E, 0x00, 0x00, 0x80, 0x93 }; // Freezer
+
+#define fridgeOpto  16 // WeMos pin D0 for fridge
+#define freezeOpto  14 // WeMos pin D5 for freezer
 
 int tempFridge, tempFreezer;
 
@@ -40,7 +41,15 @@ void setup()
 {
   Serial.begin(9600);
   Blynk.begin(auth, "ssid", "pw");
+
+  while (Blynk.connect() == false) {
+    // Wait until connected
+  }
+
   Wire.begin(4, 5); // Set pins for SDA/SCL (4=D2 for SDA and 5=D1 for SCL)
+
+  pinMode(fridgeOpto, INPUT);
+  pinMode(freezeOpto, INPUT);
 
   sensors.begin();
   sensors.setResolution(ds18b20Fridge, 9);
@@ -50,6 +59,75 @@ void setup()
   display.clearDisplay();
 
   timer.setInterval(2000L, sendTemps); // Temperature sensor polling interval
+  timer.setInterval(1000L, fridgeDoorStatus);
+
+  statusBlinkOn();
+}
+
+BLYNK_WRITE(V27) // App button to report uptime
+{
+  int pinData = param.asInt();
+
+  if (pinData == 0)
+  {
+  timer.setTimeout(8000L, uptimeSend);
+  }
+}
+
+void uptimeSend()  // Blinks a virtual LED in the Blynk app to show the ESP is live and reporting.
+{
+  float secDur = millis() / 1000;
+  float minDur = secDur / 60;
+  float hourDur = minDur / 60;
+  terminal.println(String("Refrigerator uptime: ") + hourDur + " hours ");
+  terminal.flush();
+}
+
+void fridgeDoorStatus()
+{
+  Serial.println(digitalRead(fridgeOpto));
+  Serial.println(digitalRead(freezeOpto));
+
+  if (digitalRead(freezeOpto) == HIGH && digitalRead(fridgeOpto) == HIGH)
+  {
+    Blynk.virtualWrite(25, "<Open       Open>");
+  }
+  else if (digitalRead(freezeOpto) == LOW && digitalRead(fridgeOpto) == LOW)
+  {
+    Blynk.virtualWrite(25, "<Closed  Closed>");
+  }
+    else if (digitalRead(freezeOpto) == HIGH && digitalRead(fridgeOpto) == LOW)
+  {
+    Blynk.virtualWrite(25, "<Open    Closed>");
+  }
+    else if (digitalRead(freezeOpto) == LOW && digitalRead(fridgeOpto) == HIGH)
+  {
+    Blynk.virtualWrite(25, "<Closed    Open>");
+  }
+}
+
+void statusBlinkOn()
+{
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(54, 0);
+  display.println(":");
+  display.setCursor(108, 0);
+  display.println(" ");
+  timer.setTimeout(1000L, statusBlinkOff);
+  display.display();
+}
+
+void statusBlinkOff()
+{
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(55, 0);
+  display.println(" ");
+  display.setCursor(108, 0);
+  display.println(":");
+  timer.setTimeout(1000L, statusBlinkOn);
+  display.display();
 }
 
 void loop()
@@ -65,7 +143,7 @@ void loop()
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.println("  Freezer:  Refrig:");
+  display.println("  Freezer   Refrig");
   display.setTextSize(3);
   if (tempFreezer < -9 && tempFreezer > -100) // If temp is 3-char
   {
@@ -120,6 +198,6 @@ void loop()
 
 void sendTemps()
 {
-  Blynk.virtualWrite(0, tempFreezer);
-  Blynk.virtualWrite(1, tempFridge);
+  Blynk.virtualWrite(28, tempFreezer);
+  Blynk.virtualWrite(29, tempFridge);
 }
